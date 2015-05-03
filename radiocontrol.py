8517258b493da7e0cba1a8edcdf99c512706234b
abraham@abrahamsalloum.com <abraham@abrahamsalloum.com>
2015-05-03 03:29:12 +0000

    slight fix

--- radiocontrol.py
+++ radiocontrol.py
@@ -10,11 +10,12 @@ GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 while True:
         if(GPIO.input(24) == 0):
+		GPIO.cleanup()
                 os.system("shutdown -h now")
         if(GPIO.input(23) == 0):
                 os.system("mpc prev")
         if(GPIO.input(22) == 0):
                 os.system("mpc next")
 	time.sleep(0.2); 
-GPIO.cleanup()
+
 
